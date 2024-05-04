c4fb74ff628c90825f6ff01345840cb111fc85fa
Francisco Fernandes <francisco.rmfernandes@gmail.com>
2024-05-04 21:49:35 +0100

    Update pieceToAction lists

--- pipe.py
+++ pipe.py
@@ -41,20 +41,20 @@ strToPiece: dict = {
 pieceToStr: list = ['', 'FD', 'FE', 'LH', 'FB', 'VB', 'BB', 'VE', 'FC', 'VD', 'VC', 'BC', 'LV', 'BD', 'BE']
 pieceToAction = [
                   [], # 0b0000 
-                  [], # 0b0001 
-                  [], # 0b0010 
-                  [], # 0b0011 
-                  [], # 0b0100 
-                  [], # 0b0101 
-                  [], # 0b0110 
-                  [], # 0b0111 
-                  [], # 0b1000 
-                  [], # 0b1001 
-                  [], # 0b1010 
-                  [], # 0b1011 
-                  [], # 0b1100 
-                  [], # 0b1101 
-                  [], # 0b1110 
+                  [np.uint8(0b0100), np.uint8(0b1000), np.uint8(0b0010)], # 0b0001 
+                  [np.uint8(0b0100), np.uint8(0b1000), np.uint8(0b0001)], # 0b0010 
+                  [np.uint8(0b1100)], # 0b0011 
+                  [np.uint8(0b0010), np.uint8(0b1000), np.uint8(0b0001)], # 0b0100 
+                  [np.uint8(0b0110), np.uint8(0b1010), np.uint8(0b1001)], # 0b0101 
+                  [np.uint8(0b0101), np.uint8(0b1010), np.uint8(0b1001)], # 0b0110 
+                  [np.uint8(0b1101), np.uint8(0b1110), np.uint8(0b1011)], # 0b0111 
+                  [np.uint8(0b0010), np.uint8(0b0100), np.uint8(0b0001)], # 0b1000 
+                  [np.uint8(0b0101), np.uint8(0b1010), np.uint8(0b0110)], # 0b1001 
+                  [np.uint8(0b0101), np.uint8(0b1001), np.uint8(0b0110)], # 0b1010 
+                  [np.uint8(0b1110), np.uint8(0b0111), np.uint8(0b1101)], # 0b1011 
+                  [np.uint8(0b0011)], # 0b1100 
+                  [np.uint8(0b1110), np.uint8(0b0111), np.uint8(0b1011)], # 0b1101 
+                  [np.uint8(0b1101), np.uint8(0b0111), np.uint8(0b1011)], # 0b1110 
                 ]
 
 class PipeManiaState:
@@ -104,8 +104,8 @@ class PipeMania(Problem):
       for col in range(1, state.board.matrix.side):
         piece = state.board.matrix[row, col]
         for action in pieceToAction[piece]:
-          if action != piece:
-            yield (row, col, action)
+          # if action != piece:
+          yield (row, col, action)
 
   def result(self, state: PipeManiaState, action):
     """Retorna o estado resultante de executar a 'action' sobre
